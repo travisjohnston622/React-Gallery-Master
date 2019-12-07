7124dd275ae04fb26c561ab84b479b2449f54b9e
Travis Johnston <travisjohnston4588@icloud.com>
2019-12-07 07:36:21 -0600

    gallery.data.js updated

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,35 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    {
+        id: 1, 
+        title: 'Billy goat',
+        path: 'images/goat_small.jpg',
+        description: 'Photo of a goat taken at Glacier National Park.',
+        likes: 0,
+    },
+
+    {
+        id: 2,
+        title: 'Cotton Tops',
+        path: 'Cotton Tops.jpeg',
+        description: 'Photo of a drake and hen american wigeon ducks.',
+        likes: 0
+    },
+
+    {
+        id: 3,
+        title: 'Green Head',
+        path: 'Mallard Drake.jpg',
+        description: 'Photo of a drake mallard duck.',
+        likes: 0,
+    },
+
+    {
+        id: 4,
+        title: 'Sprig',
+        path: 'Pintail Drake.jpg',
+        description: 'Photo of a drake pintail duck.',
+        likes: 0
+    },
 ];
 
 module.exports = galleryItems;
