436417aacc884e3fabc2a6d2548eeab79f674175
Travis Johnston <travisjohnston4588@icloud.com>
2019-12-08 18:43:47 -0600

    GET call & new components  created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import axios from 'axios';
+import GalleryList from '../GallaryList/GalleryList';
 class App extends Component {
+  state = {
+    galleryItems: []
+  }
+
+  componentDidMount() {
+    this.getGallery();
+  }
+
+  getGallery = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    })
+      .then((response) => {
+        this.setState({
+          galleryItems: response.data
+        }, () => {
+          console.log(this.state);
+        });
+      })
+      .catch((err) => {
+        console.warn(err);
+      })
+  }
+
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
-        <p>Gallery goes here</p>
-        <img src="images/Black_Duck.jpg"/>
-        <img src="images/Cotton_Tops.jpg" />
-        <img src="images/Mallard_Drake.jpg" />
-        <img src="images/Pintail_Drake.jpg" />
+        <br />
+        <GalleryList />
+
       </div>
     );
   }
---
+++ src/components/GallaryList/GalleryList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class GalleryList extends Component {
+    render() {
+        return (
+            <div>
+                <img src="images/Black_Duck.jpg" />
+                <img src="images/Cotton_Tops.jpeg" />
+                <img src="images/Mallard_Drake.jpg" />
+                <img src="images/Pintail_Drake.jpg" />
+            </div>
+        )
+    }
+}
+
+export default GalleryList;
